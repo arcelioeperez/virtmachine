c465247275a124b093e9e3407372e96a87a37e16
aep labs <aepzgar@gmail.com>
2021-10-18 10:05:56 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// Registers
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+// Opcodes
+enum {
+    OP_BR = 0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+};
+
+enum {
+
+};
+
+int main(){
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,6 @@
+!/usr/bin/env bash
+
+echo "Compiling ..." 
+clang main.c
+echo "Running ..."
+./a.out
